79b41b54127fbd43a95ce2c9128662f29119a52a
Jeongsu0880 <latsyrc900@gmail.com>
2025-11-26 12:58:09 +0900

    trythis

---
+++ ts/trythis/array.ts
@@ -0,0 +1,26 @@
+const enum ID {
+    XS = 'XS',
+    S = 'S',
+    M = 'M',
+    L = 'L',
+    XL = 'XL'
+}
+
+type PriceofSize = { id: ID, price: number }
+
+const SIZE: PriceofSize[] = [
+    { id: ID.XS, price: 8000 },
+    { id: ID.S, price: 10000 },
+    { id: ID.M, price: 12000 },
+    { id: ID.L, price: 14000 },
+    { id: ID.XL, price: 15000 },
+]
+
+const sizeOption = { XS: 1, S: 5, M: 2, L: 2, XL: 4 };
+
+const sizeOption1 = { XS: 1, S: 5, M: 2, L: 2, XL: 4 };
+const totalPrice1 = SIZE.reduce((currPrice, size) => currPrice + sizeOption1[size.id] * size.price, 0);
+
+const sizeOption2 = { XS: 2, S: 3, MM: 4, L: 5, XL: 6 };
+const totalPrice2 = SIZE.reduce((currPrice, size) => currPrice + sizeOption2[size.id] * size.price, 0);
+//두번째일때 에러가 나게 하라!!
